c769b501037971e7112011ae2a158757e8b6ba40
Eco <osw2156@gmail.com>
2023-02-25 16:00:52 -0600

    asteriscos

---
+++ Loops/asterisco.java
@@ -0,0 +1,15 @@
+public class asterisco {
+    public static void main(String[] args) {
+    
+        int i, j;
+        int n = 3;
+    
+        for(i = 1; i <= n; ++i){ 
+            for(j = 1; j <= i; ++j){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+ }
+
