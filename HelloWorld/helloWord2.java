ce61c9d59ce3d0c8b4af6e6fcdb20bc47f7bce85
Eco <osw2156@gmail.com>
2023-03-01 00:18:54 -0600

    Update helloWord2.java

--- HelloWorld/helloWord2.java
+++ HelloWorld/helloWord2.java
@@ -5,7 +5,20 @@ public class helloWord2 {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Hola Mundo");
+        String[] letter = {"h","e","l","l","o","w","o","r","l","d"};
 
+        String letter_Save = "";
+
+        for (int a = 0; a < letter.length; a++) {
+            for (char b = 'a'; b <= 'z'; b++) {
+                if (letter[a].equals(Character.toString(b))) {
+                    letter_Save += b;
+                    System.out.println(letter_Save);
+                    break;
+                } else {
+                    System.out.println(letter_Save + b);
+                }
+            }
+        }
     }
 }
