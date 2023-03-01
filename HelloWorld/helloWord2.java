f9275730466e319e0a9c0e36a2b51db7e1f2f032
Eco <osw2156@gmail.com>
2023-02-28 23:54:34 -0600

    Init

---
+++ HelloWorld/helloWord2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class helloWord2 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hola Mundo");
+
+    }
+}
